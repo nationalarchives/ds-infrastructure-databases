802e33e909f51d34a87baa2cc0029c2c06334398
karlkern <karl.kern@nationalarchives.gov.uk>
2023-09-01 07:50:24 +0100

    remove lookups completely

--- components/terraform/security-groups/private-beta/main.tf
+++ components/terraform/security-groups/private-beta/main.tf
@@ -42,9 +42,9 @@ resource "aws_vpc_security_group_egress_rule" "cidr" {
     security_group_id = aws_security_group.postgres.id
 
     for_each = var.egress
-    cidr_ipv4   = lookup(each.value, "cidr_ipv4", "")
-    description = lookup(each.value, "description", "")
-    from_port   = lookup(each.value, "from_port", "")
-    ip_protocol = lookup(each.value, "ip_protocol", "")
-    to_port     = lookup(each.value, "to_port", "")
+    cidr_ipv4   = each.value["cidr_ipv4"]
+    description = each.value["description"]
+    from_port   = each.value["from_port"]
+    ip_protocol = each.value["ip_protocol"]
+    to_port     = each.value["to_port"]
 }
