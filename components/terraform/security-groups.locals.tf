384219b97716e2ef7628cac373628c2023356c24
karlkern <karl.kern@nationalarchives.gov.uk>
2023-09-01 07:44:48 +0100

    cidr_ipv4

--- components/terraform/security-groups.locals.tf
+++ components/terraform/security-groups.locals.tf
@@ -8,21 +8,21 @@ locals {
             "to_port"     = 5432
             "ip_protocol" = "tcp"
             "description" = "private subnet a"
-            "cidr_blocks" = data.aws_ssm_parameter.private_subnet_a_cidr.value
+            "cidr_ipv4" = data.aws_ssm_parameter.private_subnet_a_cidr.value
         }
         "priv_b_sub" = {
             "from_port"   = 5432
             "to_port"     = 5432
             "ip_protocol" = "tcp"
             "description" = "private subnet b"
-            "cidr_blocks" = data.aws_ssm_parameter.private_subnet_b_cidr.value
+            "cidr_ipv4" = data.aws_ssm_parameter.private_subnet_b_cidr.value
         }
         "client_vpn" = {
             "from_port"   = 5432
             "to_port"     = 5432
             "ip_protocol" = "tcp"
             "description" = "client-vpn"
-            "cidr_blocks" = data.aws_ssm_parameter.client_vpc_cidr.value
+            "cidr_ipv4" = data.aws_ssm_parameter.client_vpc_cidr.value
         }
     }
 
@@ -41,9 +41,7 @@ locals {
             "to_port"     = 65535
             "ip_protocol" = "tcp"
             "description" = "to everywhere"
-            "cidr_blocks" = [
-                "0.0.0.0/0"
-            ]
+            "cidr_ipv4" = "0.0.0.0/0"
         }
     }
 }
