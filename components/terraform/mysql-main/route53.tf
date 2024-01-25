228d1a144d2752b35fe5dd56df498488bc2955e1
karlkern <karl.kern@nationalarchives.gov.uk>
2024-01-25 13:53:33 +0000

    remove cname setting

--- components/terraform/mysql-main/route53.tf
+++ components/terraform/mysql-main/route53.tf
@@ -3,9 +3,7 @@ resource "aws_route53_record" "instance" {
     name    = var.mysql_dns
     type    = "CNAME"
     ttl     = 5
-
-    set_identifier = "mysql"
-    records        = [
+    records = [
         aws_instance.mysql_main.private_dns,
     ]
 }
