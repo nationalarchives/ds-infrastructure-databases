c07da7be8e6ce1ed69f6d66068dee3d2fa5167cc
karlkern <karl.kern@nationalarchives.gov.uk>
2024-01-25 13:34:37 +0000

    set cname record

--- components/terraform/mysql-main/route53.tf
+++ components/terraform/mysql-main/route53.tf
@@ -1,11 +1,11 @@
 resource "aws_route53_record" "instance" {
     zone_id = var.zone_id
-    name    = "mysql-main-${var.resource_identifier}"
+    name    = var.mysql_dns
     type    = "CNAME"
     ttl     = 5
 
     set_identifier = "mysql"
     records        = [
-        var.mysql_dns,
+        aws_instance.mysql_main.private_dns,
     ]
 }
