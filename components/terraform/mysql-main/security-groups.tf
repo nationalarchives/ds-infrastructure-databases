53e45b406d5613350e1f9e94dff7c0f9d8c4e4a6
karlkern <karl.kern@nationalarchives.gov.uk>
2024-02-11 21:15:14 +0000

    userdata and ami security group

--- components/terraform/mysql-main/security-groups.tf
+++ components/terraform/mysql-main/security-groups.tf
@@ -40,3 +40,35 @@ resource "aws_vpc_security_group_egress_rule" "all_egress" {
     ip_protocol = "-1"
     to_port     = 0
 }
+
+resource "aws_security_group" "mysql_main_ami" {
+    name        = "mysql-main-${var.resource_identifier}-ami-sg"
+    description = "allow access to prime for ami build"
+    vpc_id      = var.vpc_id
+
+    lifecycle {
+        create_before_destroy = true
+    }
+
+    tags = merge(var.tags, {
+        Name = "mysql-main-${var.resource_identifier}-sg"
+    })
+}
+
+resource "aws_vpc_security_group_ingress_rule" "ami_ingress" {
+    security_group_id = aws_security_group.mysql_main_ami.id
+
+    referenced_security_group_id = aws_security_group.mysql_main.id
+    from_port                    = 3306
+    ip_protocol                  = "tcp"
+    to_port                      = 3306
+}
+
+resource "aws_vpc_security_group_egress_rule" "ami_egress" {
+    security_group_id = aws_security_group.mysql_main.id
+
+    referenced_security_group_id = aws_security_group.mysql_main.id
+    from_port   = 1024
+    ip_protocol = "tcp"
+    to_port     = 65535
+}
