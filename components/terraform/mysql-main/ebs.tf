a8baea09e3044b361d144cf6d431da9676c456d1
karlkern <karl.kern@nationalarchives.gov.uk>
2024-02-16 13:30:04 +0000

    commnet out ebs

--- components/terraform/mysql-main/ebs.tf
+++ components/terraform/mysql-main/ebs.tf
@@ -1,10 +1,10 @@
-resource "aws_ebs_volume" "mysql_main_ebs_prime" {
-    availability_zone = var.mysql_main_availability_zone
-    encrypted         = true
-    final_snapshot    = true
-    size              = var.mysql_main_ebs_volume_size
-
-    tags = {
-        Name = "mysql-main-ebs-${var.resource_identifier}"
-    }
-}
+#resource "aws_ebs_volume" "mysql_main_ebs_prime" {
+#    availability_zone = var.mysql_main_availability_zone
+#    encrypted         = true
+#    final_snapshot    = true
+#    size              = var.mysql_main_ebs_volume_size
+#
+#    tags = {
+#        Name = "mysql-main-ebs-${var.resource_identifier}"
+#    }
+#}
