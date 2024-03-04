32fdee2999c6835aeabcaf96baefe6569e2365c2
karlkern <karl.kern@nationalarchives.gov.uk>
2024-03-04 17:51:27 +0000

    add vars

--- components/terraform/postgres-main.tf
+++ components/terraform/postgres-main.tf
@@ -2,3 +2,9 @@ variable "private_beta_description" {}
 variable "private_beta_volume_size" {}
 variable "private_beta_instance_type" {}
 variable "private_beta_s3_folder" {}
+variable "private_beta_disable_api_stop" {}
+variable "private_beta_disable_api_termination" {}
+variable "private_beta_availability_zone" {}
+variable "private_beta_ebs_delete_on_termination" {}
+variable "private_beta_volume_type" {}
+variable "private_beta_key_name" {}
