b23a584d928c291ef6eba3274cd9b99aa8470732
karlkern <karl.kern@nationalarchives.gov.uk>
2024-03-10 22:35:27 +0000

    refactor

--- components/terraform/postgres-main/scripts/userdata.sh
+++ components/terraform/postgres-main/scripts/userdata.sh
@@ -2,38 +2,51 @@
 
 sudo dnf update -y
 
-sudo touch /start-up.log
+sudo touch /var/log/start-up.log
 
+BASE_DIR="/postgres"
+DATA_DIR="/postgres/data"
+LOG_DIR="/postgres/log"
 # mounting process for ebs
-echo "$(date '+%Y-%m-%d %T') - check if postgres directory exist" | sudo tee -a /start-up.log > /dev/null
-if [ ! -d "/postgres" ]; then
-  echo "$(date '+%Y-%m-%d %T') - create postgres directory" | sudo tee -a /start-up.log > /dev/null
-  sudo mkdir /postgres
-  sudo chown -R postgres:postgres /postgres
+echo "$(date '+%Y-%m-%d %T') - check if postgres directory exist" | sudo tee -a /var/log/start-up.log > /dev/null
+if [ ! -d "$BASE_DIR" ]; then
+  echo "$(date '+%Y-%m-%d %T') - create postgres directory" | sudo tee -a /var/log/start-up.log > /dev/null
+  sudo mkdir $BASE_DIR
+  sudo chown -R postgres:postgres $BASE_DIR
 else
-  echo "$(date '+%Y-%m-%d %T') - postgres directory found" | sudo tee -a /start-up.log > /dev/null
+  echo "$(date '+%Y-%m-%d %T') - postgres directory found" | sudo tee -a /var/log/start-up.log > /dev/null
 fi
 
-echo "$(date '+%Y-%m-%d %T') - check if EBS is mounted" | sudo tee -a /start-up.log > /dev/null
-mounted=$(df -h --type=xfs | grep /postgres)
+echo "$(date '+%Y-%m-%d %T') - check if EBS is mounted" | sudo tee -a /var/log/start-up.log > /dev/null
+mounted=$(df -h --type=xfs | grep $BASE_DIR)
 if [ -z "${mounted}" ]; then
-  echo "$(date '+%Y-%m-%d %T') - prepare system for persistent mounting of EBS" | sudo tee -a /start-up.log > /dev/null
+  echo "$(date '+%Y-%m-%d %T') - prepare system for persistent mounting of EBS" | sudo tee -a /var/log/start-up.log > /dev/null
   mntDriveID="$(sudo blkid /dev/xvdf | grep -oP 'UUID="(.*?)"' | grep -oP '"(.*?)"' | sed 's/"//g')"
   if [[ -z ${mntDriveID} ]]; then
-    sudo echo "$(date '+%Y-%m-%d %T') - error mounting EBS" | sudo tee -a /start-up.log > /dev/null
+    sudo echo "$(date '+%Y-%m-%d %T') - error mounting EBS" | sudo tee -a /var/log/start-up.log > /dev/null
   else
-    echo "$(date '+%Y-%m-%d %T') - set up system for persistent mounting of EBS" | sudo tee -a /start-up.log > /dev/null
-    sudo echo "UUID=$mntDriveID  /postgres  xfs  defaults,nofail  0  2" | sudo tee -a /etc/fstab > /dev/null
-    echo "$(date '+%Y-%m-%d %T') - mount of EBS" | sudo tee -a /start-up.log > /dev/null
-    sudo mount /dev/xvdf /postgres
-    echo "$(date '+%Y-%m-%d %T') - sync data to EBS" | sudo tee -a /start-up.log > /dev/null
-    sudo rsync -av /var/lib/pgsql/data /postgres
-    echo "$(date '+%Y-%m-%d %T') - set up log directory on EBS" | sudo tee -a /start-up.log > /dev/null
-    sudo mkdir /postgres/log
-    sudo chown -R postgres:postgres /postgres/log
-    sudo chmod 0700 /postgres/log
+    echo "$(date '+%Y-%m-%d %T') - set up system for persistent mounting of EBS" | sudo tee -a /var/log/start-up.log > /dev/null
+    sudo echo "UUID=$mntDriveID  $BASE_DIR  xfs  defaults,nofail  0  2" | sudo tee -a /etc/fstab > /dev/null
+    echo "$(date '+%Y-%m-%d %T') - mount of EBS" | sudo tee -a /var/log/start-up.log > /dev/null
+    sudo mount /dev/xvdf $BASE_DIR
+    echo "$(date '+%Y-%m-%d %T') - check if data directory exists" | sudo tee -a /var/log/start-up.log > /dev/null
+    if [ ! -d "$DATA_DIR" ]; then
+      echo "$(date '+%Y-%m-%d %T') - sync data to EBS" | sudo tee -a /var/log/start-up.log > /dev/null
+      sudo rsync -av /var/lib/pgsql/data $BASE_DIR
+    else
+      echo "$(date '+%Y-%m-%d %T') - data directory exists" | sudo tee -a /var/log/start-up.log > /dev/null
+    fi
+    echo "$(date '+%Y-%m-%d %T') - check if log directory exists" | sudo tee -a /var/log/start-up.log > /dev/null
+    if [ ! -d "$LOG_DIR" ]; then
+      echo "$(date '+%Y-%m-%d %T') - set up log directory on EBS" | sudo tee -a /var/log/start-up.log > /dev/null
+      sudo mkdir $LOG_DIR
+      sudo chown -R postgres:postgres $LOG_DIR
+      sudo chmod 0700 $LOG_DIR
+    else
+      echo "$(date '+%Y-%m-%d %T') - log directory exists" | sudo tee -a /var/log/start-up.log > /dev/null
+    fi
     sudo systemctl start postgresql
   fi
 else
-  echo "$(date '+%Y-%m-%d %T') - EBS found)" | sudo tee -a /start-up.log > /dev/null
+  echo "$(date '+%Y-%m-%d %T') - EBS found)" | sudo tee -a /var/log/start-up.log > /dev/null
 fi
