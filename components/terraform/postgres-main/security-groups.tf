94df03af7d1de3a00215283320819065d8f53199
karlkern <karl.kern@nationalarchives.gov.uk>
2024-03-12 08:22:45 +0000

    set correct port

--- components/terraform/postgres-main/security-groups.tf
+++ components/terraform/postgres-main/security-groups.tf
@@ -16,9 +16,9 @@ resource "aws_vpc_security_group_ingress_rule" "self_ingress" {
     security_group_id = aws_security_group.postgres_main.id
 
     referenced_security_group_id = aws_security_group.postgres_main.id
-    from_port                    = 3306
+    from_port                    = 5432
     ip_protocol                  = "tcp"
-    to_port                      = 3306
+    to_port                      = 5432
 }
 
 resource "aws_vpc_security_group_ingress_rule" "db_port_ingress" {
@@ -27,9 +27,9 @@ resource "aws_vpc_security_group_ingress_rule" "db_port_ingress" {
     security_group_id = aws_security_group.postgres_main.id
 
     cidr_ipv4   = var.db_subnet_cidrs[count.index]
-    from_port   = 3306
+    from_port   = 5432
     ip_protocol = "tcp"
-    to_port     = 3306
+    to_port     = 5432
 }
 
 resource "aws_vpc_security_group_egress_rule" "http_egress" {
