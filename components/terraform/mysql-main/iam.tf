ce126b23ca1be2c40b0a0198a7c7a41b450151dc
Steven Hirschorn <steve.hirschorn@gmail.com>
2025-02-20 16:27:26 +0000

    Add org-session-manager-logs group to instance profile role

--- components/terraform/mysql-main/iam.tf
+++ components/terraform/mysql-main/iam.tf
@@ -32,7 +32,8 @@ resource "aws_iam_role" "mysql_main_role" {
         "arn:aws:iam::aws:policy/CloudWatchAgentServerPolicy",
         aws_iam_policy.mysql_main_deployment_source_access_policy.arn,
         aws_iam_policy.mysql_main_backup_policy.arn,
-        var.attach_ebs_volume_policy_arn
+        var.attach_ebs_volume_policy_arn,
+        "arn:aws:iam::${var.account_id}:policy/org-session-manager-logs"
     ]
 
     tags = var.tags
