3b1af44a2a44c3039ff20f5b2c508fb0862be906
Steven Hirschorn <steve.hirschorn@gmail.com>
2026-03-24 12:10:48 +0000

    Pin provider version as recommended in documentation...
    ...https://registry.terraform.io/providers/mongodb/mongodbatlas/latest/docs#version-requirements

--- components/terraform/provider.tf
+++ components/terraform/provider.tf
@@ -4,5 +4,9 @@ terraform {
   # TODO Consider if this is the correct provider version
   required_providers {
       aws = ">= 5.14.0"
+      mongodbatlas = {
+        source  = "mongodb/mongodbatlas"
+        version = "~> 2.0"
+      }
   }
 }
