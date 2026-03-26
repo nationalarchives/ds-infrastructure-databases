f3faacde1b08becc0441036af06c4037bd7f8879
Steven Hirschorn <steve.hirschorn@gmail.com>
2026-03-26 11:03:49 +0000

    Add MongoDB resources

--- components/terraform/data.tf
+++ components/terraform/data.tf
@@ -60,8 +60,16 @@ data "aws_ssm_parameter" "private_subnet_a_cidr" {
     name = "/infrastructure/private_subnet_2a_cidr"
 }
 
+data "aws_ssm_parameter" "private_subnet_a_id" {
+    name = "/infrastructure/network/base/private_subnet_2a_id"
+}
+
 data "aws_ssm_parameter" "private_subnet_b_cidr" {
     name = "/infrastructure/private_subnet_2b_cidr"
+}
+
+data "aws_ssm_parameter" "private_subnet_b_id" {
+    name = "/infrastructure/network/base/private_subnet_2b_id"
 }
 
 data "aws_ssm_parameter" "private_db_subnet_a_cidr" {
---
+++ components/terraform/mongodb/data.tf
@@ -0,0 +1,51 @@
+# ------------------------------------------------------------------------------
+# MongoDB sources
+# ------------------------------------------------------------------------------
+
+data "aws_ssm_parameter" "atlas_platform_team_id" {
+    provider = aws.intersite
+    name = "atlas_platform_team_id"
+}
+
+data "aws_ssm_parameter" "atlas_developer_team_id" {
+    provider = aws.intersite
+    name = "atlas_developer_team_id"
+}
+
+data "aws_ssm_parameter" "mongodb_org_id" {
+    provider = aws.intersite
+    name = "atlas_org_id"
+}
+
+# ------------------------------------------------------------------------------
+# Kew CIDR Ranges
+# ------------------------------------------------------------------------------
+data "aws_ssm_parameter" "kew_developer_network" {
+    provider = aws.intersite
+    name = "/infrastructure/on-prem/networks/kew_developer_network"
+}
+
+data "aws_ssm_parameter" "kew_app_network" {
+    provider = aws.intersite
+    name = "/infrastructure/on-prem/networks/kew_app_network"
+}
+
+data "aws_ssm_parameter" "kew_new_test_app_network" {
+    provider = aws.intersite
+    name = "/infrastructure/on-prem/networks/kew_new_test_app_network"
+}
+
+data "aws_ssm_parameter" "kew_new_live_app_network" {
+    provider = aws.intersite
+    name = "/infrastructure/on-prem/networks/kew_new_live_app_network"
+}
+
+data "aws_ssm_parameter" "kew_wvd_network" {
+    provider = aws.intersite
+    name = "/infrastructure/on-prem/networks/kew_wvd_network"
+}
+
+data "aws_ssm_parameter" "london_client_vpn_network" {
+    provider = aws.intersite
+    name = "/infrastructure/networks/vpn/dsintersite_vpc_cidr"
+}
