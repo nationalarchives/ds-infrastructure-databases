08f6bcf760905d5fbcb79cbadfd41c75a7a159d4
karlkern <karl.kern@nationalarchives.gov.uk>
2026-09-10 13:35:15 +0100

    update iam policy attachment code

--- components/terraform/mysql-main/iam.tf
+++ components/terraform/mysql-main/iam.tf
@@ -26,17 +26,7 @@ resource "aws_iam_policy" "mysql_main_backup_policy" {
 resource "aws_iam_role" "mysql_main_role" {
     name               = "mysql-${var.resource_identifier}-role"
     assume_role_policy = file("${path.root}/templates/assume-role-ec2-policy.json")
-
-    managed_policy_arns = [
-        "arn:aws:iam::aws:policy/AmazonSSMManagedInstanceCore",
-        "arn:aws:iam::aws:policy/CloudWatchAgentServerPolicy",
-        aws_iam_policy.mysql_main_deployment_source_access_policy.arn,
-        aws_iam_policy.mysql_main_backup_policy.arn,
-        var.attach_ebs_volume_policy_arn,
-        "arn:aws:iam::${var.account_id}:policy/org-session-manager-logs"
-    ]
-
-    tags = var.tags
+    tags               = var.tags
 }
 
 resource "aws_iam_instance_profile" "mysql_main_profile" {
@@ -44,4 +34,29 @@ resource "aws_iam_instance_profile" "mysql_main_profile" {
     role = aws_iam_role.mysql_main_role.name
 
     tags = var.tags
+}
+
+resource "aws_iam_role_policy_attachment" "ssm_managed_instance_core" {
+    role       = aws_iam_role.mysql_main_role.name
+    policy_arn = "arn:aws:iam::aws:policy/AmazonSSMManagedInstanceCore"
+}
+resource "aws_iam_role_policy_attachment" "cloudwatch_agent_server_policy" {
+    role       = aws_iam_role.mysql_main_role.name
+    policy_arn = "arn:aws:iam::aws:policy/CloudWatchAgentServerPolicy"
+}
+resource "aws_iam_role_policy_attachment" "deployment_source_access_policy" {
+    role       = aws_iam_role.mysql_main_role.name
+    policy_arn = aws_iam_policy.mysql_main_deployment_source_access_policy.arn
+}
+resource "aws_iam_role_policy_attachment" "backup_policy" {
+    role       = aws_iam_role.mysql_main_role.name
+    policy_arn = aws_iam_policy.mysql_main_backup_policy.arn
+}
+resource "aws_iam_role_policy_attachment" "attach_ebs_volume_policy" {
+    role       = aws_iam_role.mysql_main_role.name
+    policy_arn = var.attach_ebs_volume_policy_arn
+}
+resource "aws_iam_role_policy_attachment" "org_session_manager_logs" {
+    role       = aws_iam_role.mysql_main_role.name
+    policy_arn = "arn:aws:iam::${var.account_id}:policy/org-session-manager-logs"
 }
